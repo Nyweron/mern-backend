10716173e63c8a22fee61c4f34cc7f2e613b2dc4
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-05 17:01:35 +0200

    Added Place-Specific Routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+const dummyPlaces = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famouse sky scraper in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = dummyPlaces.find(p=>{
+    return p.id === placeId;
+  })
   console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+  res.json({ place });
 });
 
 module.exports = router;
