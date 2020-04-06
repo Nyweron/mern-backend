3e55742f0e97d8c0fbe874f7527812d981a9b4f0
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-06 21:34:13 +0200

    Fixed small bug. Return all places by user ID

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -34,20 +34,20 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = dummyPlaces.find((p) => {
+  const places = dummyPlaces.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided user id.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -88,7 +88,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlaceById = updatePlaceById;
 exports.deletePlace = deletePlace;
