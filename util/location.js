f60642cb4c80342774a2066cc34bfb1320bda4d3
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-08 20:35:06 +0200

    Modified createPlace to async

---
+++ util/location.js
@@ -0,0 +1,10 @@
+const API_KEY = "";
+
+//dummy function
+async function getCoordsForAddress(address) {
+  /*
+   I don't want use any Geocoding API to Convert an Address Into Coordinates, for this backend.
+  maybe try use this: https://opencagedata.com/api#quickstart  (it's free)
+  */
+  return { lat: 40.7484474, lng: -739871516 };
+}
