8ff6300b29af00b47b8db8547f6d9f7f8f08eabb
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-18 21:16:00 +0200

    Changed lat and lng for static Schwabing, Munchen

--- util/location.js
+++ util/location.js
@@ -6,7 +6,7 @@ async function getCoordsForAddress(address) {
    I don't want use any Geocoding API to Convert an Address Into Coordinates, for this backend.
   maybe try use this: https://opencagedata.com/api#quickstart  (it's free)
   */
-  return { lat: 40.7484474, lng: -739871516 };
+  return { lat: 48.1599, lng: 11.5761 };
 }
 
 module.exports = getCoordsForAddress;
