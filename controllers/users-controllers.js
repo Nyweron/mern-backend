9fa9cca41b954c743cba4741f5975654ec76691b
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-18 19:42:03 +0200

    Return logged user object

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -80,7 +80,10 @@ const login = async (req, res, next) => {
     );
   }
 
-  res.json({ message: "Logged in" });
+  res.json({
+    message: "Logged in",
+    user: existingUser.toObject({ getters: true }),
+  });
 };
 
 exports.getUsers = getUsers;
