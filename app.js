7627000ff40b6346f18aa2e38093192d474b4de2
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-25 18:49:53 +0200

    Gave access to uploaded images

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -17,6 +18,8 @@ const database = process.env.DATABASE;
 const password = process.env.PASS;
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
