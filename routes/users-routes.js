cc4191821ce85610262d73f1053a5afffc251862
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-25 17:30:13 +0200

    Filtered files (images Only)

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 
+const fileUpload = require('../middleware/file-upload');
 const usersControllers = require("../controllers/users-controllers");
 
 const router = express.Router();
@@ -9,6 +10,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
