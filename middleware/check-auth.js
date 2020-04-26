688975ad81769c41ebfa0c3b6553b30493332e0f
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-26 14:54:01 +0200

    Fixed some bug

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 const secretToken = process.env.TOKEN;
 
 module.exports = (req, res, next) => {
+  if(req.method === 'OPTIONS') {
+    return next();
+  }
+
   try {
     const token = req.headers.authorization.split(" ")[1]; //Authorization: 'Bearer TOKEN'
     if (!token) {
