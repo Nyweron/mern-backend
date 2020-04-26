b38464b80c219483858df6b4ddfc36dc7fd60349
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-26 14:40:23 +0200

    Backend Route Protection with Auth Middleware

---
+++ middleware/check-auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const HttpError = require("../models/http-error");
+
+require("dotenv").config();
+const secretToken = process.env.TOKEN;
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; //Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+
+    const decodedToken = jwt.verify(token, secretToken);
+    req.userData = {userId: decodedToken.userId}
+    next();
+
+  } catch (err) {
+    return next(new HttpError("Authentication failed!", 401));
+  }
+};
